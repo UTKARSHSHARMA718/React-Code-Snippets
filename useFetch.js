e296d3dcd399754351a63b544bcce4c562b45c20
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2023-09-16 15:23:14 +0530

    Create useFetch.js

---
+++ useFetch.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const getData = async () => {
+      setData(null);
+      setError("");
+      setIsLoading(true);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw "Unable to fetch data.";
+        }
+        const resJson = await res.json();
+        setData(resJson);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err);
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, []);
+  return [data, error, isLoading];
+};
